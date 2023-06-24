0287118a1a380abdaf9932e5febbbbf4c7ae4a16
YamalAli770 <yamalali770@gmail.com>
2023-06-24 12:05:27 +0500

    updated whitelist options

--- server.js
+++ server.js
@@ -23,10 +23,8 @@ connectDB();
 
 const app = express();
 
-const whiteList = ['http://localhost:5173', "https://stripe.com", "https://zawiyah-frontend.onrender.com/"];
-
 const corsOptions = {
-    origin: whiteList,
+    origin: ['http://localhost:5173', "https://stripe.com", "https://zawiyah-frontend.onrender.com/"],
     credentials: true,
 }
 
