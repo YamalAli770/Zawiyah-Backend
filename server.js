9a2e7eefceaa35f5de0ae217d2741e2e173acf73
YamalAli770 <yamalali770@gmail.com>
2023-06-24 12:13:37 +0500

    allowed cors request from all origins

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173', "https://stripe.com", "https://zawiyah-frontend.onrender.com/"],
+    origin: '*',
     credentials: true,
 }
 
