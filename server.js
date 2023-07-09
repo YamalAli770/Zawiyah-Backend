7037297549261cc2a3888200b8e9da9d4200618c
Yamal Ali <yamalali770@gmail.com>
2023-07-09 22:59:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-    origin: ['https://zawiyah.netlify.app', 'http://localhost:5173'],
+    origin: ['https://zawiyah.netlify.app', 'http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials: true,
 }
 
